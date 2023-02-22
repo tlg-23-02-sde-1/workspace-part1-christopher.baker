80c7249d5ebd1439cb261826a5c2fc1b2504843a
Christopher Baker <christopher.baker@tlgcohort.com>
2023-02-22 13:42:27 -0800

    Lab 10.1 Complete

--- Lab10.1/src/com/hr/personnel/client/HRClient.java
+++ Lab10.1/src/com/hr/personnel/client/HRClient.java
@@ -9,9 +9,9 @@
 package com.hr.personnel.client;
 
 import com.hr.personnel.Department;
-import com.hr.personnel.Employee;
 import com.hr.personnel.HourlyEmployee;
 import com.hr.personnel.SalariedEmployee;
+
 import java.time.LocalDate;
 
 /**
@@ -26,7 +26,7 @@ class HRClient {
 
         // add Employees to it
         dept.addEmployee(new SalariedEmployee("Jason",  LocalDate.of(1990, 8, 24), 2250.0));
-        dept.addEmployee(new HourlyEmployee("Julie",    LocalDate.of(2000, 2, 2), 35.0, 30.0));
+        dept.addEmployee(new HourlyEmployee("Julie",    LocalDate.of(2000, 2, 2), 15.01, 30.0));
         dept.addEmployee(new SalariedEmployee("Amilia", LocalDate.of(1999, 2, 6), 1250.0));
         dept.addEmployee(new HourlyEmployee("Logan",    LocalDate.of(2002, 6, 2), 40.0, 40.0));
 
